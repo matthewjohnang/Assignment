4e29995b345c5c10396e739c62f1ef75c72f8244
Matthew Ang <43584492+matthewjohnang@users.noreply.github.com>
2018-10-10 14:20:54 -0500

    Add files via upload

---
+++ 13/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 5; i < 15001; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Please enter any key to exit..");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 14/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          	string message = "hello";
+            
+            for (int i = 1; i <201; i++)
+            {
+                 Console.WriteLine(message);
+            }
+            Console.WriteLine("Please enter any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
