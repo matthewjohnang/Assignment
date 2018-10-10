e82c8d1be67298376c5e68e7b526f71bef039450
Matthew Ang <43584492+matthewjohnang@users.noreply.github.com>
2018-10-10 14:26:40 -0500

    Add files via upload

---
+++ 15/program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            for (int i = 0; i<30; i++)
+            {  
+                var sum = i + 1;
+                var multiples = sum * sum;
+                Console.WriteLine(multiples);
+            }
+            
+
+        }
+    }
+}
---
+++ 16/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a number:");
+            string input = Console.ReadLine();
+            int n;
+            Int32.TryParse(input, out n);
+            var sum = (n*(n+1))/2;
+ 			Console.WriteLine(sum);
+           
+
+           
+
+        }
+    }
+}
