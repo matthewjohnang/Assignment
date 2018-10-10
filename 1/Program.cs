61799ab680329f2c010d1ba449149eccaa940e83
Matthew Ang <43584492+matthewjohnang@users.noreply.github.com>
2018-10-10 13:55:06 -0500

    Add files via upload

---
+++ 1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           	Console.WriteLine("Hi, I already print sentences.");
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
