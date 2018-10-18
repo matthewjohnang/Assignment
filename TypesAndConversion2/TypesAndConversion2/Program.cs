6900961b30e70298f59feaffd0d393b800efa1ba
Matthew Ang <43584492+matthewjohnang@users.noreply.github.com>
2018-10-18 10:38:52 -0500

    Add files via upload

---
+++ TypesAndConversion2/TypesAndConversion2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace TypesAndConversion2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = GetFirstName();
+            string secondName = GetSecondName();
+            string result = GetFirst (firstName, secondName);
+            Console.WriteLine("The name " + result + " is first.");
+        }
+
+        static string GetFirstName()
+        {
+            Console.WriteLine("Please enter first name:");
+            String firstName = Console.ReadLine();
+            return firstName;
+        }
+        static string GetSecondName()
+        {
+            Console.WriteLine("Please enter second name:");
+            String secondName = Console.ReadLine();
+            return secondName;
+        }
+        static string GetFirst(string firstName, string secondName)
+        {
+            int i = 0;
+            while ((int)firstName[i] == (int)secondName[i])
+            {
+                i++;
+            }
+
+            if ((int)firstName[i] < (int)secondName[i])
+            {
+                return firstName;
+            }
+            else
+            {
+                return secondName;
+            }
+
+        }
+
+    }
+}
+
+
---
+++ TypesAndConversion3/TypesAndConversion3/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace TypesAndConversion2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = GetFirstName();
+            string secondName = GetSecondName();
+            string result = GetFirst(firstName, secondName);
+            Console.WriteLine("The name " + result + " is first.");
+        }
+
+        static string GetFirstName()
+        {
+            Console.WriteLine("Please enter first name:");
+            String iFirstName = Console.ReadLine();
+            string firstName = iFirstName.ToLower();
+            return firstName;
+        }
+        static string GetSecondName()
+        {
+            Console.WriteLine("Please enter second name:");
+            String iSecondName = Console.ReadLine();
+            String secondName = iSecondName.ToLower();
+            return secondName;
+        }
+        static string GetFirst(string firstName, string secondName)
+        {
+            int i = 0;
+            while ((int)firstName[i] == (int)secondName[i])
+            {
+                i++;
+            }
+
+            if ((int)firstName[i] < (int)secondName[i])
+            {
+                return firstName;
+            }
+            else
+            {
+                return secondName;
+            }
+
+        }
+
+    }
+}
+
+
